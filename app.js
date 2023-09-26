e0f1175be1bff91f2d20f364d565b522aa266832
arfernn <arturo@Arturos-Air.lan>
2023-09-26 03:24:26 +0300

    adding first working mobile layout

---
+++ app.js
@@ -0,0 +1,31 @@
+const submitBtnId = "#submit-btn";
+const dismissBtnId = "#dismiss-btn";
+
+let form = document.querySelector("#sub-form");
+
+let submitBtn = document.querySelector("#submit-btn");
+let dismissBtn = document.querySelector("#dismiss-btn");
+
+let primaryView = document.querySelector("primary");
+let secondaryView = document.querySelector("secondary");
+
+submitBtn.addEventListener("click",(e)=> {
+    // Prevent page refreshing
+    e.preventDefault();
+
+    if(!form.checkValidity())
+    {
+        form.reportValidity();
+    }
+    else {
+        primaryView.style.display = "none";
+        secondaryView.style.display = "block";
+    }
+});
+
+dismissBtn.addEventListener("click",(e)=>{
+    // Prevent page refreshing
+    e.preventDefault();
+    primaryView.style.display = "block";
+    secondaryView.style.display = "none";    
+});
